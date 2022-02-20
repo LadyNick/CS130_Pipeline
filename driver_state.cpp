d1c45a14df404934e40a5f0eb5588c134c46ac25
Nicole Navarro <nnava026@ti-05.cs.ucr.edu>
2022-02-20 03:01:50 -0800

    Checkpt 1 done

--- driver_state.cpp
+++ driver_state.cpp
@@ -1,5 +1,6 @@
 #include "driver_state.h"
 #include <cstring>
+using namespace std;
 
 driver_state::driver_state()
 {
@@ -22,7 +23,7 @@ void initialize_render(driver_state& state, int width, int height)
     state.image_color= new pixel[width*height];
     state.image_depth= new float[width*height];
 
-    for(int i = 0; i < width*height; i++){
+    for(int i = 0; i < (width*height); i++){
 
         //this initializes all pixels in image_color to black
         state.image_color[i] = make_pixel(0,0,0);
@@ -43,12 +44,41 @@ void initialize_render(driver_state& state, int width, int height)
 void render(driver_state& state, render_type type)
 {
     std::cout<<"TODO: implement rendering."<<std::endl;
+    
+    switch(type){
+        case render_type::triangle:{
+            
+            //first step: allocal an array of data_geometry objects, one for each vertex
+            data_geometry objs[3];//there is 3 vertices per triangle
+            data_vertex vertex[3]; // 3 vertices per triangle, so 1 data_vertex per vertice
+            for (int i =0 ; i<3 ; ++i){ //we're going to go through each vertex in a loop and allocate the data member float* data
+                //for each vertex i, we're going to fill in for each of the j floats in each vertex
+                //j is incremented by 3*state.floats_per_vertex because we are going by vertex
+                for (int j = 0 ; j< state.num_vertices*state.floats_per_vertex; j += 3 *state.floats_per_vertex){
+                    vertex[i].data= &state.vertex_data[(state.floats_per_vertex * i) + j];
+                }
+                //we need to fill in the contents of the objects, using vertex shader
+                state.vertex_shader(vertex[i], objs[i], state.uniform_data);
+                //we now have on data_gepmetry object for each vertex fully filled
+            }
+            //call rasterize_triangle to rasterize the triangle
+            rasterize_triangle(state, objs[0], objs[1], objs[2]); 
+        }
+        break;
+        //leave the remaining cases empty because we don't need them for now
+        case render_type::indexed:{}
+        break;
+        case render_type::strip: {}
+        break;
+        case render_type::fan: {}
+        break;
+    }
 }
 
 
 // This function clips a triangle (defined by the three vertices in the "in" array).
 // It will be called recursively, once for each clipping face (face=0, 1, ..., 5) to
-// clip against each of the clipping faces in turn.  When face=6, clip_triangle should
+// clip against each of the clipping faces in turn.  When face=6, cli triangle should
 // simply pass the call on to rasterize_triangle.
 void clip_triangle(driver_state& state, const data_geometry& v0,
     const data_geometry& v1, const data_geometry& v2,int face)
@@ -69,5 +99,44 @@ void rasterize_triangle(driver_state& state, const data_geometry& v0,
     const data_geometry& v1, const data_geometry& v2)
 {
     std::cout<<"TODO: implement rasterization"<<std::endl;
+
+    //first we fill out the coordinates for each vertex
+    float A_x = ((state.image_width/2) * v0.gl_Position[0]);
+    float A_y = ((state.image_height/2) * v0.gl_Position[1]);
+    float B_x = ((state.image_width/2) * v1.gl_Position[0]);
+    float B_y = ((state.image_height/2) * v1.gl_Position[1]);
+    float C_x = ((state.image_width/2) * v2.gl_Position[0]);
+    float C_y = ((state.image_height/2) * v2.gl_Position[1]);
+    //these are the coordinates for point P where i is x and j is y
+    float  P_i = (state.image_width/2) - 0.5;
+    float  P_j = (state.image_height/2) - 0.5;
+    //These are the coordinates that relate the vertices to the point P coordinates
+    //we need to transfer the data_geometry positions from NDC's to pixel coordinates
+    float Ax_i = A_x + P_i;
+    float Ay_j = A_y + P_j;
+    float Bx_i = B_x + P_i;
+    float By_j = B_y + P_j;
+    float Cx_i = C_x + P_i;
+    float Cy_j = C_y + P_j;
+    //this was a given formula from the lab sheet
+    float AREA_ABC = 0.5 * (((Bx_i * Cy_j - (Cx_i * By_j)) - ((Ax_i* Cy_j) - (Cx_i * Ay_j)) + ((Ax_i* By_j) - (Bx_i * Ay_j))));
+
+    //we have a forloop for triangle rasterization for all x from xmin to xmax, with a forloop for all y from ymin to ymax
+    //i put using namespace std at the top so the max and min functions would work
+    for (int i = min(min(Ax_i,Bx_i),Cx_i); i < max(max(Ax_i,Bx_i),Cx_i); i++){
+        for (int j=min(min(Ay_j,By_j),Cy_j); j<max(max(Ay_j,By_j),Cy_j) ; j++){
+            //for alpha beta and gamma you just use the same equation for ABC but swap out the letter youre solving for with P
+            //or in this case, the i and j's which are going through each min to max values
+            float alpha = 0.5 * (((Bx_i * Cy_j)-(Cx_i * By_j))-(( i * Cy_j) - (Cx_i * j))+(( i * By_j)-(Bx_i * j))) /AREA_ABC;
+            float beta = 0.5 * ((( i * Cy_j)-(Cx_i * j))-((Ax_i * Cy_j) - (Cx_i * Ay_j))+((Ax_i * j)-( i * Ay_j))) /AREA_ABC;
+            float gamma = 0.5 * (((Bx_i * j)-( i * By_j))-((Ax_i * j)-( i * Ay_j))+((Ax_i * By_j) - (Bx_i * Ay_j))) /AREA_ABC;
+        //this implies 
+        if (alpha >=0 && beta >=0 && gamma <=1){
+            int index = (state.image_width  * j) + i;
+            state.image_color[index] = make_pixel(255,255,255);
+        }
+        }
+   }
+
 }
 
